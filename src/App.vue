77cc06fffddc6ba775f0e9c3c1468ce9d5a64e7c
yurimarcon <yuri.erik.oliveira@gmail.com>
2021-09-20 22:59:21 -0300

    upload do projeto

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Item from './components/Item.vue'
+</script>
+
+<script>
+import Dados from './Dados/Services.json';
+
+export default {
+  data(){
+    return{
+      servicos: null
+    }
+  },
+  mounted(){
+    this.servicos = Dados;
+    // console.log(Dados);
+  }
+}
+</script>
+
+<template>
+  <img alt="Vue logo" style="width:70px" src="./assets/logo.png" />
+  <h1>Seja bem vindo(a)!</h1>
+  <hr/>
+  <div class="items">
+    <Item 
+    v-for="servico, index in servicos"
+    :key="index"
+    :item="servico"
+    />
+
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+html{
+  background: url("https://cdn.pixabay.com/photo/2017/01/24/03/53/plant-2004483_960_720.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100%;
+}
+.items{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,49 @@
+<template>
+    <div
+    class="card"
+    >
+        <a
+        :href="item.href"
+        >
+            <img :src="item.img" alt="Icone do servico" />
+        </a>
+        <span>{{ item.titulo }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['item']
+}
+</script>
+
+<style scoped>
+div{
+    margin: 20px 50px 20px 50px;
+    display: block;
+    width: 100px;
+}
+img{
+    width: 80px;
+}
+span{
+    font-weight: 900;
+}
+.card{
+    display: flex ;
+    justify-content: center;
+    flex-direction: column;
+    min-width: 120px;
+    min-height: 150px;
+    border: 1px solid grey;
+    border-radius: 5%;
+    opacity: .9;
+    box-shadow: 0 0 2em grey;
+    transition: box-shadow .5s;
+}
+.card:hover{
+    opacity: 1;
+    box-shadow: 0 0 4em grey;
+}
+
+</style>
